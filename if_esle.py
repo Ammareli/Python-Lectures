a03642357b23d7f93a3b79ff40d9b4e64c4a1d10
Ammareli <ammarali.ak12@gmail.com>
2025-10-24 00:06:55 +0500

    if else and comparsion operators.

---
+++ if_esle.py
@@ -0,0 +1,78 @@
+# COMPARISON OPERATORS: if-else statement example
+# == (equal to)
+# != (not equal to)
+# > (greater than)
+# < (less than)
+# >= (greater than or equal to)
+# <= (less than or equal to)
+
+a = 4
+b = 10
+print(a >= b)  # False
+
+
+
+# IF ELSE STATEMENT
+"""
+    syntax:
+    if condition:
+        # code to be executed if condition is true
+    else:
+        # code to be executed if condition is false
+
+"""
+
+
+
+
+
+
+
+
+
+number = int(input("Enter a number: "))
+
+if (number > 5):
+
+    print("The number is greater than 5")
+
+else:
+
+    print("The number is less than to 5")
+
+
+
+
+# elif statement
+num = int(input("Enter a number: "))
+if (num > 0):
+    print("The number is positive")
+elif (num == 0):
+    print("The number is zero")
+else:
+    print("The number is negative")
+
+
+
+# TASK 
+"""
+input number 1: 10
+
+selct form oprtions(+, -, *, /, %, //):+
+
+enter number 2: 3
+The sum is: 13
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
