5c600c2578f3ca85001b2d5c88efa3276f925aa0
Ammareli <ammarali.ak12@gmail.com>
2025-10-23 23:30:45 +0500

    added input.py

---
+++ input.py
@@ -0,0 +1,30 @@
+# a = input("Enter first number: ")
+
+# b = input("Enter 2nd number: ")
+
+# a = int(a) 
+
+# b = int(b)
+
+# sum = a + b 
+# print("The sum is:", sum)
+# multiplication = a * b
+# print("The multiplication is:", multiplication)
+# div = a / b
+# print("The division is:", div)
+# mod = a % b
+# print("The modulus is:", mod)
+# floor_div = a // b
+# print("The floor division is:", floor_div)
+
+# TASK 2
+
+#  hello name.
+
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
+
+
+
+
+
